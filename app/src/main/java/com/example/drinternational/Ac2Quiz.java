a282b74c27e11b4ffeab6252f499d9262cc54c8d
val vec <val.vec@hotmail.com>
2022-04-26 17:05:58 +0200

    street view, ki nima api kljuca

--- app/src/main/java/com/example/drinternational/Ac2Quiz.java
+++ app/src/main/java/com/example/drinternational/Ac2Quiz.java
@@ -1,16 +1,34 @@
 package com.example.drinternational;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Ac2Quiz extends AppCompatActivity {
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class Ac2Quiz extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ac2_quiz);
+        SupportStreetViewPanoramaFragment streetViewPanoramaFragment =
+                (SupportStreetViewPanoramaFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+
     }
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        LatLng sanFrancisco = new LatLng(37.754130, -122.447129);
+        streetViewPanorama.setPosition(sanFrancisco);
+    }
+
+
 }
 
 //wazzzupps
