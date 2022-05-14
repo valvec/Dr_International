cad99276c3b7a13dd7a6e0fbf80bec68d5fa2c86
val vec <val.vec@hotmail.com>
2022-05-14 17:20:21 +0200

    Random location, ampak samo avstirja pa ceska

--- app/src/main/java/com/example/drinternational/Ac2Quiz.java
+++ app/src/main/java/com/example/drinternational/Ac2Quiz.java
@@ -10,23 +10,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
 import com.google.android.gms.maps.StreetViewPanorama;
-import com.google.android.gms.maps.StreetViewPanoramaOptions;
 import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaLocation;
 import com.google.android.gms.maps.model.StreetViewSource;
-import com.google.android.material.tabs.TabLayout;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
 
-public class Ac2Quiz extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback, View.OnClickListener {
+public class Ac2Quiz<streetViewPanoramaCamera> extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback, View.OnClickListener, StreetViewPanorama.OnStreetViewPanoramaChangeListener {
     String countryName;
     int currentIndex=0;
     int totalQuestion = map.koordinate.length;
@@ -102,7 +102,7 @@ public class Ac2Quiz extends AppCompatActivity implements OnStreetViewPanoramaRe
         ansD.setVisibility(View.VISIBLE);
 
         if(currentIndex == totalQuestion ){
-            finishQuiz();
+            generateMap();
             return;
         }
 
@@ -122,7 +122,7 @@ public class Ac2Quiz extends AppCompatActivity implements OnStreetViewPanoramaRe
         }
         //
 
-        streetViewPanorama1.setPosition(map.koordinate[currentIndex],StreetViewSource.OUTDOOR);
+        streetViewPanorama1.setPosition(map.koordinate[currentIndex],10000,StreetViewSource.OUTDOOR);
         streetViewPanorama1.setStreetNamesEnabled(false);
         streetViewPanorama1.setUserNavigationEnabled(false);
         ansA.setText(map.allCountries[selectionIndexesABCD[0]]);
@@ -135,7 +135,7 @@ public class Ac2Quiz extends AppCompatActivity implements OnStreetViewPanoramaRe
         if (randomNum2 >= 1 && randomNum2 < 2) ansB.setText(countryName);
         if (randomNum2 >= 2 && randomNum2 < 3) ansC.setText(countryName);
         if (randomNum2 >= 3 && randomNum2 <= 4) ansD.setText(countryName);
-
+        streetViewPanorama1.setOnStreetViewPanoramaChangeListener(this);
     }
 
     void finishQuiz(){
@@ -214,6 +214,36 @@ public class Ac2Quiz extends AppCompatActivity implements OnStreetViewPanoramaRe
         Toast toast =Toast.makeText(this,"-50", Toast.LENGTH_SHORT);
         toast.show();
     }
+
+
+    @Override
+    public void onStreetViewPanoramaChange(@NonNull StreetViewPanoramaLocation streetViewPanoramaLocation) {
+        if (streetViewPanoramaLocation != null && streetViewPanoramaLocation.links != null) {
+            Toast.makeText(Ac2Quiz.this, "StreetView Camera Found", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(Ac2Quiz.this, "StreetView Camera not Found", Toast.LENGTH_SHORT).show();
+            naloziLokacijo();
+        }
+
+    }
+
+    void generateMap(){
+
+        for(currentIndex=0;currentIndex<4;currentIndex++){
+            double lat=(Math.random() * 2)+ 47;
+            double longi=(Math.random() * 2) +14;
+
+        map.koordinate[currentIndex]=new LatLng(lat,longi);
+        Toast.makeText(Ac2Quiz.this, String.valueOf(lat) + "  "+String.valueOf(longi), Toast.LENGTH_SHORT).show();
+
+        }
+        currentIndex=0;
+
+    }
+
+
+
+
 }
 
 
